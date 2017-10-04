51059ae476ca0dd553220cf25c73a0eb14a099de
Héctor Tortosa <hector.tortosa@unit4.com>
2017-10-04 22:35:38 +0200

    Define initializers before use them

--- RecorderFactory.py
+++ RecorderFactory.py
@@ -1,14 +1,14 @@
 from Recorders import Recorder, PrintRecorder, FileRecorder
 
-factory = dict([
-    ('print', create_print_recorder),
-    ('file', create_file_recorder)])
-
-def create_recorder(config):
-    return factory[config.type](config.config)
-
 def create_print_recorder(config):
     return PrintRecorder(config)
 
 def create_file_recorder(config):
     return FileRecorder(config)
+
+recorderInitializers = dict([
+    ('print', create_print_recorder),
+    ('file', create_file_recorder)])
+
+def create_recorder(config):
+    return recorderInitializers[config.type](config.config)
